30bf2bf5c7058a419298cc825c96b9537929556a
Charles Limousin <contact@charleslimousin.com>
2025-05-05 16:02:57 +0200

    Fix the name of the source directory

--- rinit.sh
+++ rinit.sh
@@ -1,19 +1,19 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    rinit                                              :+:      :+:    :+:    #
+#    rinit.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/08 18:36:58 by chlimous          #+#    #+#              #
-#    Updated: 2024/04/16 16:21:37 by chlimous         ###   ########.fr        #
+#    Updated: 2025/05/05 14:30:53 by chlimous         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 cp $(dirname "$(readlink -f "$0")")/.gitignore $(dirname "$(readlink -f "$0")")/Makefile .
-mkdir -p src
+mkdir -p source
 mkdir -p include
 git clone https://github.com/chlimous/42-libft_enhanced libft
 rm -rf libft/libft.png libft/README.md libft/LICENSE libft/.gitignore
