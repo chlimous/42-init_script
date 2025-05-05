a0d777763be2331a64d8138498e1e97a2acf5ef0
Charles Limousin <contact@charleslimousin.com>
2025-05-05 19:33:36 +0200

    Change install directory

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
@@ -6,13 +8,22 @@
 #    By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/08 18:41:10 by chlimous          #+#    #+#              #
-#    Updated: 2024/05/03 00:46:21 by chlimous         ###   ########.fr        #
+#    Updated: 2025/05/05 17:47:52 by chlimous         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-rm -rf ~/bin/rinit ~/bin/rinitd
-mkdir -p ~/bin/rinitd
-cp .gitignore Makefile rinit.sh ~/bin/rinitd
-ln -s ~/bin/rinitd/rinit.sh ~/bin/rinit
-chmod +x ~/bin/rinitd/rinit.sh
-echo 'export PATH="$HOME/bin:$PATH"' >> $HOME/.`basename $SHELL`rc
+BIN_NAME=rinit
+BIN_DIR="${XDG_BIN_HOME:-$HOME/.local/bin}"
+DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/$BIN_NAME"
+
+mkdir -p $DATA_DIR
+cp .gitignore Makefile $DATA_DIR
+
+mkdir -p $BIN_DIR
+cp $BIN_NAME $BIN_DIR
+chmod +x $BIN_DIR/$BIN_NAME
+SHELL_RC="$HOME/.`basename $SHELL`rc"
+PATH_LINE="export PATH=\"$BIN_DIR:\$PATH\""
+if ! grep -Fxq "$PATH_LINE" "$SHELL_RC"; then
+	echo "$PATH_LINE" >> "$SHELL_RC"
+fi
